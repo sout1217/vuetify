4e8a83ca09ba97ee865b94517023dc2c6da4c585
hbh <sout1217@naver.com>
2021-08-08 16:03:05 +0900

    v-slot 이해하기

---
+++ src/views/components/app/ButtonTab.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="btn-div">
+    <div class="btn-tab">
+      <!-- header -->
+      <slot name="header"></slot>
+
+      <!-- content -->
+      <span>Content</span>
+
+      <!-- nav -->
+      <nav>
+        <slot name="nav"></slot>
+      </nav>
+
+      <!-- bind -->
+      <slot name="img" :childData="childData"> </slot>
+
+      <!-- default -->
+      <slot></slot>
+
+      <slot name="age" :childData="childData" :something="somethingData"></slot>
+      <slot name="gender" v-bind="somethingData"></slot>
+      <!-- footer -->
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonTab',
+  data() {
+    return {
+      childData: {
+        username: 'martin',
+        age: 14,
+        gender: 'male',
+      },
+      somethingData: {
+        message: 'Hello Something',
+        message2: 'Hello Anything',
+      },
+    }
+  },
+}
+</script>
+
+<style scoped>
+.btn-tab {
+  background-color: blueviolet;
+  display: inline-flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+}
+</style>
--- src/views/page/Dashboard.vue
+++ src/views/page/Dashboard.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>홈 페이지</div>
+  <div>
+    홈 페이지
+    <button-tab>
+      <h1>Default</h1>
+      <span slot="footer">Footer</span>
+      <!-- = <template v-slot:default></template>> -->
+      <template v-slot:nav> Nav </template>
+      <template v-slot:img="prop">
+        {{ prop.childData.username }}
+      </template>
+      <template v-slot:age="{ childData, something }">
+        <p>{{ childData.age }} | {{ something.message }}</p>
+      </template>
+      <template #gender="{ message2 }"> {{ message2 }}</template>
+      <span slot="header">Header</span>
+    </button-tab>
+  </div>
 </template>
 
 <script>
+import ButtonTab from '@/views/components/app/ButtonTab'
 export default {
   name: 'Dashboard',
+  components: { ButtonTab },
   data() {
     return {}
   },
